655077afeca20a8d0d8377098b5ffb9dabbc03f5
TalhaGH <thussain1123@gmail.com>
2017-08-29 22:47:40 +0200

    Login via Firebase UI

--- android/app/src/main/java/com/interestallofus/interestus/LoginActivity.java
+++ android/app/src/main/java/com/interestallofus/interestus/LoginActivity.java
@@ -9,18 +9,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
-
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
-    
 
     private static final int RC_SIGN_IN = 0;
     private FirebaseAuth auth;
@@ -29,27 +23,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Set up the login form.
 
-        auth = FirebaseAuth.getInstance();
-        if (auth.getCurrentUser() != null) {
-            //user signed in
-            Log.d("AUTH", auth.getCurrentUser().getEmail());
-        }else{
-
-
-            startActivityForResult(AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .setProviders(
-                            AuthUI.FACEBOOK_PROVIDER,
-                            AuthUI.EMAIL_PROVIDER,
-                            AuthUI.GOOGLE_PROVIDER)
-                    .build(), 1);
-        }
-
+        findViewById(R.id.email_sign_in_button).setOnClickListener(this);
         findViewById(R.id.sign_out_button).setOnClickListener(this);
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -57,6 +34,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if (requestCode == RC_SIGN_IN){
             if(resultCode == RESULT_OK){
                 Log.d("AUTH", auth.getCurrentUser().getEmail());
+                //Add Intent to go to next activity after successful login
+                //Intent goToMenu = new Intent(LoginActivity.this, MainMenu.class)
             }
             else{
                 Log.d("AUTH", "NOT AUTHENTICATED");
@@ -66,17 +45,44 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
-        if(view.getId() == R.id.sign_out_button){
-            AuthUI.getInstance()
-                    .signOut(this)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            Log.d("AUTH", "USER LOGGED OUT");
-                        }
-                    });
+        switch(view.getId()){
+            case R.id.email_sign_in_button:
+                signIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
         }
     }
+
+    private void signIn()
+    {
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            Log.d("AUTH", auth.getCurrentUser().getEmail());
+        }else{
+            startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setProviders(
+                            AuthUI.FACEBOOK_PROVIDER,
+                            AuthUI.EMAIL_PROVIDER,
+                            AuthUI.GOOGLE_PROVIDER)
+                    .build(), 1);
+        }
+    }
+
+    private void signOut()
+    {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d("AUTH", "USER LOGGED OUT");
+                    }
+                });
+    }
+
 }
 
 
